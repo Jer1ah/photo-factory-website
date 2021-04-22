5f35d44916f7506716423b642135a5ef56486569
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-04-21 23:26:03 -0400

    working on image sort functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // image navigation functionality 
 const imageNavigationController = (() => {
+
     const _navLinks = document.querySelectorAll('.photo-navigation__listItem');
+    const _galleryItems = document.querySelectorAll('.gallery__listItem');
+
 
     _navLinks.forEach((item) => {
         item.addEventListener('click', () => {
@@ -11,4 +14,60 @@ const imageNavigationController = (() => {
             item.classList.add('activeLink');
         });
     });
+
+    _navLinks[0].addEventListener('click', () => {
+        _galleryItems[0].style.display = "none";
+    });
+
+})();
+
+// image shuffle sort functionality
+const imageShuffleController = (() => {
+
+    const _navLinks = document.querySelectorAll('.photo-navigation__listItem');
+    const _galleryItems = document.querySelectorAll('.gallery__listItem');
+
+    const filterImageList = (keyword) => {
+        for(let i = 0; i < _galleryItems.length; i++) {
+            if(_galleryItems[i].classList.contains(keyword)) {
+                _galleryItems[i].style.display = "inline-block";
+            } else {
+                _galleryItems[i].style.display = "none";
+            }
+        }
+    };
+
+    _navLinks.forEach((item) => {
+        item.addEventListener('click', () => {
+            switch(item.firstElementChild.innerHTML.toLocaleLowerCase()) {
+                case 'all':
+                    filterImageList('all');
+                    return;
+                case 'art':
+                    filterImageList('art');
+                    return;
+                case 'commercial':
+                    filterImageList('commercial');
+                    return;
+                case 'design':
+                    filterImageList('design');
+                    return;
+                case 'misc':
+                    filterImageList('misc');
+                    return;
+                case 'aesthetic':
+                    filterImageList('aesthetic');
+                    return;
+                case 'photography':
+                    filterImageList('photography');
+                    return;
+                case 'tech':
+                    filterImageList('tech');
+                    return;
+                default:
+                    return;
+            }
+        });
+    });
+
 })();
